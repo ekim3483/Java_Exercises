c421b671251da74efe8cfdb558c5905172bbd5a8
ekim3483 <ekim3483@gmail.com>
2025-11-12 18:24:01 -0800

    Cat

--- Meowstructor/CatAccess.java
+++ Meowstructor/CatAccess.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class CatAccess {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Cat myCat = new Cat();
 
