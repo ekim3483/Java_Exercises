59ad3720cc310d669b5f45202c2df6232b5583c6
ekim3483 <ekim3483@gmail.com>
2025-11-13 18:51:20 -0800

    Start the virtual coffee machine
    Updated the main method to allow multiple cups of coffee to be poured until a limit is reached. Added user input functionality to trigger pouring a cup.

--- Caffeine/Instance.java
+++ Caffeine/Instance.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 public class Instance{
     public static void main(String[] args) {
-        // Pour a cup
-        Cupof joe1 = new Cupof();
 
-        if (Cupof.getCoffee() < 4) {
-            System.out.println("Here's your caffeine fix:");
-            System.out.println("   J    a   v     v  a ");
-            System.out.println("   J   a a   v   v  a a");
-            System.out.println("J  J  aaaaa   V V  aaaaa");
-            System.out.println(" JJ  a     a   V  a     a");
+        Scanner pour = new Scanner(System.in);
+
+        while (Cupof.getCoffee() < 4){
+            System.out.println("Press any key to pour a cup");
+
+            // Pour a cup
+
+            String cups = pour.nextLine();
+
+            if (cups != null) {
+                Cupof joe = new Cupof();
+
+                System.out.println("Here's your caffeine fix!");
+                System.out.println("");
+                System.out.println("   J    a   v     v  a ");
+                System.out.println("   J   a a   v   v  a a");
+                System.out.println("J  J  aaaaa   V V  aaaaa");
+                System.out.println(" JJ  a     a   V  a     a");
+                System.out.println("");
+            }
         }
-        System.out.println("That's enough Java for today. I'll add a menu tomorrow.");
-
+        System.out.println("That's enough Java for today.");
     }
 }
